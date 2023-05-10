b742c2fd04651e30fb4fa0e1df7f30ca2feaceb5
dnr1112 <dnr1112@naver.com>
2023-05-10 14:11:16 +0900

    oauth2 back init

---
+++ src/main/java/com/study/oauth2/controller/AuthController.java
@@ -0,0 +1,9 @@
+package com.study.oauth2.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+	
+}
---
+++ src/main/java/com/study/oauth2/security/OAuth2SuccessHandler.java
@@ -0,0 +1,24 @@
+package com.study.oauth2.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
+	
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		
+		System.out.println((OAuth2User)authentication.getPrincipal());
+	}
+
+}
